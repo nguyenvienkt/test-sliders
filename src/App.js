ba56dc83d4ff551245e601b9fa00a081b7e15fca
nguyenvienkt <nguyenvienkt@gmail.com>
2021-01-12 14:51:49 +0700

    slide

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import Articles from "./screen/components/Articles";
+import Sliders from "./screen/components/Sliders";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [showComponent, setShowComponent] = useState(true);
+  // const [slides, setSlide] = useState([
+  //   {
+  //     title: "Today's workout plan",
+  //     text: "We're gonna do 3 fundamental exercises.",
+  //   },
+  //   {
+  //     title: "First, 10 push-ups",
+  //     text: "Do 10 reps. Remember about full range of motion. Don't rush.",
+  //   },
+  //   {
+  //     title: "Next, 20 squats",
+  //     text: "Squats are important. Remember to keep your back straight.",
+  //   },
+  //   {
+  //     title: "Finally, 15 sit-ups",
+  //     text: "Slightly bend your knees. Remember about full range of motion.",
+  //   },
+  //   {
+  //     title: "Great job!",
+  //     text: "You made it, have a nice day and see you next time!",
+  //   },
+  // ]);
+
+  // let pageNumber = [];
+  // for (var i = 0; i < slides.length; i++) {
+  //   pageNumber.push(i);
+  // }
+
+  const ARTICLES = [
+    {
+      title: "A message to our customers",
+      upvotes: 12,
+      date: "2020-01-24",
+    },
+    {
+      title: "Alphabet earnings",
+      upvotes: 22,
+      date: "2019-11-23",
+    },
+    {
+      title: "Artificial Mountains",
+      upvotes: 2,
+      date: "2019-11-22",
+    },
+    {
+      title: "Scaling to 100k Users",
+      upvotes: 72,
+      date: "2019-01-21",
+    },
+    {
+      title: "the Emu War",
+      upvotes: 24,
+      date: "2019-10-21",
+    },
+    {
+      title: "What's SAP",
+      upvotes: 1,
+      date: "2019-11-21",
+    },
+    {
+      title: "Simple text editor has 15k monthly users",
+      upvotes: 7,
+      date: "2010-12-31",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <a href="#">K-Soft</a>
+            </li>
+          </ul>
+          <ul>
+            <li onClick={() => setShowComponent(true)}>Sliders</li>
+            <li onClick={() => setShowComponent(false)}>Articles</li>
+          </ul>
+        </nav>
       </header>
+      {showComponent ? <Sliders /> : <Articles />}
+
+      <div></div>
+      <hr />
+      {/* {pageNumber.map((item, index) => {
+        return (
+          <ul>
+            <li>{item + 1}</li>
+          </ul>
+        );
+      })} */}
     </div>
   );
 }
---
+++ src/screen/components/Sliders.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+const Sliders = (props) => {
+  const slides = [
+    {
+      title: "Today's workout plan",
+      text: "We're gonna do 3 fundamental exercises.",
+    },
+    {
+      title: "First, 10 push-ups",
+      text: "Do 10 reps. Remember about full range of motion. Don't rush.",
+    },
+    {
+      title: "Next, 20 squats",
+      text: "Squats are important. Remember to keep your back straight.",
+    },
+    {
+      title: "Finally, 15 sit-ups",
+      text: "Slightly bend your knees. Remember about full range of motion.",
+    },
+    {
+      title: "Great job!",
+      text: "You made it, have a nice day and see you next time!",
+    },
+  ];
+  // const { slides } = props;
+  let pageNumber = [];
+  for (var i = 0; i < slides.length; i++) {
+    pageNumber.push(i);
+  }
+
+  // pageNumber.map((item, index) => {
+  //   return (
+  //     <ul>
+  //       <li>{item + 1}</li>
+  //     </ul>
+  //   );
+  // });
+
+  const [slide, setSlide] = useState(0);
+  const [isShow, setIsShow] = useState(false);
+
+  const nextSlide = () => {
+    setSlide(slide + 1);
+  };
+  const previousSlide = () => {
+    setSlide(slide - 1);
+  };
+  return (
+    <div>
+      <h1>Sliders</h1>
+      <div className="sliders">
+        <h3>{slides[slide].title}</h3>
+        <p>{slides[slide].text}</p>
+      </div>
+
+      <div className="controls">
+        <button
+          className="btn"
+          onClick={() => {
+            setSlide(0);
+          }}
+        >
+          Restart
+        </button>
+
+        <button
+          onClick={() => {
+            previousSlide();
+          }}
+          className={"btn " + (slide === 0 ? "disable" : "")}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => {
+            nextSlide();
+          }}
+          className={"btn " + (slide === slides.length - 1 ? "disable" : "")}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sliders;
